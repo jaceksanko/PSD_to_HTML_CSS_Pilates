3d88b3a7b850672df1547509acf0acf1fd3efb93
Jacek Sanko <jacek.sanko@gmail.com>
2018-03-16 18:48:47 +0100

    Add section pilates info

--- js/script.js
+++ js/script.js
@@ -1,42 +1,31 @@
 const iconsMenu = document.getElementById('menu-icon');
 const iconOpenClose = document.getElementById('js-menu-icon-open-close');
+//const menulist = document.querySelectorAll('nav ul');
 const menuList1 = document.getElementById('js-menu1');
 const menuList2 = document.getElementById('js-menu2');
 
 iconOpenClose.addEventListener('click', function(){
 
     if (this.classList.contains('line-menu-icon-move') == false) {
+        //menulist[0].classList.remove('fadeOutUp');
+        //menulist[1].classList.remove('fadeOutUp');
+        menuList1.classList.remove('fadeOutUp');
+        menuList2.classList.remove('fadeOutUp');
         this.classList.toggle('line-menu-icon-move');
-        menuList1.style.display = 'flex';
+        //menuList[0].classList.toggle('fadeInDown');
+        //menuList[1].classList.toggle('fadeInDown');
         menuList1.classList.toggle('fadeInDown');
-        menuList2.style.display = 'flex';
         menuList2.classList.toggle('fadeInDown');
     }
     else {
-        this.classList.remove('line-menu-icon-move');
+        /* menuList[0].classList.remove('fadeInDown');
+        menuList[1].classList.remove('fadeInDown');
+        menuList[0].classList.toggle('fadeOutUp');
+        menuList[1].classList.toggle('fadeOutUp'); */
         menuList1.classList.remove('fadeInDown');
         menuList2.classList.remove('fadeInDown');
-      
-        menuList1.style.display = 'none';
-        menuList2.style.display = 'none';
-    }
-});
-
-window.addEventListener('resize', function(){ 
-    if (window.matchMedia("(min-width: 992px)").matches) {
-        iconsMenu.style.display = 'none';
-        menuList1.style.display = 'flex';
-        menuList2.style.display = 'flex';
-    }
-    else {
-        iconsMenu.style.display = 'block';
-        if (iconOpenClose.classList.contains('line-menu-icon-move') == false) {
-            menuList1.style.display = 'none';
-            menuList2.style.display = 'none';
-        }
-        else {
-            menuList1.style.display = 'flex';
-            menuList2.style.display = 'flex';
-        }
+        menuList1.classList.toggle('fadeOutUp');
+        menuList2.classList.toggle('fadeOutUp');
+        this.classList.remove('line-menu-icon-move');
     }
 });
